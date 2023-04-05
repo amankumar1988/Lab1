a8f561f694e7bad30f8efd9032645c9cff59162c
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-05 12:10:54 +0530

    Auto Push

--- test.sh
+++ test.sh
@@ -3,10 +3,10 @@
 ACTION=$1
 
 if [ "$ACTION" == "start" ];then
-   echo "Service is Starting"
+   echo "Service is Starting but mention service name also "
 
 elif [ "$ACTION" == "stop" ];then
-   echo "Service is Stopping"
+   echo "Service is Stopping but mention service name also "
 
 else
    echo "Service status is unknown"
